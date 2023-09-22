3797a659d2cf51e3695b374de9a8eb23e27a73c7
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2023-09-22 16:55:54 -0700

    compile

--- src/test/java/com/github/steveice10/mc/protocol/packet/login/serverbound/ServerboundHelloPacketTest.java
+++ src/test/java/com/github/steveice10/mc/protocol/packet/login/serverbound/ServerboundHelloPacketTest.java
@@ -3,6 +3,8 @@ package com.github.steveice10.mc.protocol.packet.login.serverbound;
 import com.github.steveice10.mc.protocol.packet.PacketTest;
 import org.junit.Before;
 
+import java.util.UUID;
+
 public class ServerboundHelloPacketTest extends PacketTest {
     @Before
     public void setup() {
