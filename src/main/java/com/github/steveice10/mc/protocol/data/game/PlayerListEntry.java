0bec1cc0bc16e74faa645fa8a46ec4919029bbd0
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2023-10-05 17:10:54 -0700

    convenience methods and constructor

--- src/main/java/com/github/steveice10/mc/protocol/data/game/PlayerListEntry.java
+++ src/main/java/com/github/steveice10/mc/protocol/data/game/PlayerListEntry.java
@@ -26,6 +26,14 @@ public class PlayerListEntry {
     private byte @Nullable[] keySignature;
 
     public PlayerListEntry(UUID profileId) {
-        this(profileId, new GameProfile(profileId, null), false, 0, GameMode.SURVIVAL, null, null, 0, null, null);
+        this(profileId, new GameProfile(profileId, null), true, 0, GameMode.SURVIVAL, null, null, 0, null, null);
+    }
+
+    public PlayerListEntry(String name, UUID profileId) {
+        this(profileId, new GameProfile(profileId, name), true, 0, GameMode.SURVIVAL, null, null, 0, null, null);
+    }
+
+    public String getName() {
+        return this.profile.getName();
     }
 }
