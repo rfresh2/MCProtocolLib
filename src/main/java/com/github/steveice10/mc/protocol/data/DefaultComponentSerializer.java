f1d861005a5201500bdf2080bd0c04d444015f7c
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2023-12-20 17:55:57 -0800

    set kyori 4.15.0 feature flags for <1.20.3

--- src/main/java/com/github/steveice10/mc/protocol/data/DefaultComponentSerializer.java
+++ src/main/java/com/github/steveice10/mc/protocol/data/DefaultComponentSerializer.java
@@ -1,11 +1,13 @@
 package com.github.steveice10.mc.protocol.data;
 
 import net.kyori.adventure.text.serializer.gson.GsonComponentSerializer;
-import net.kyori.adventure.text.serializer.gson.legacyimpl.NBTLegacyHoverEventSerializer;
+import net.kyori.adventure.text.serializer.json.JSONOptions;
+import net.kyori.adventure.text.serializer.json.legacyimpl.NBTLegacyHoverEventSerializer;
 
 public final class DefaultComponentSerializer {
     private static GsonComponentSerializer serializer = GsonComponentSerializer.builder()
             .legacyHoverEventSerializer(NBTLegacyHoverEventSerializer.get())
+            .options(JSONOptions.byDataVersion().at(2526))
             .build();
 
     public static GsonComponentSerializer get() {
