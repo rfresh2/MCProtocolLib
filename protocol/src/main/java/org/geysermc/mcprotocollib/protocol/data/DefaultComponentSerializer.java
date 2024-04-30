ce491d12001e7ca99a83f775544fb50b95defdf8
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2024-04-30 12:02:50 -0700

    set adventure compatibility level to 1.20.4

--- protocol/src/main/java/org/geysermc/mcprotocollib/protocol/data/DefaultComponentSerializer.java
+++ protocol/src/main/java/org/geysermc/mcprotocollib/protocol/data/DefaultComponentSerializer.java
@@ -1,12 +1,14 @@
 package org.geysermc.mcprotocollib.protocol.data;
 
 import net.kyori.adventure.text.serializer.gson.GsonComponentSerializer;
+import net.kyori.adventure.text.serializer.json.JSONOptions;
 import net.kyori.adventure.text.serializer.json.legacyimpl.NBTLegacyHoverEventSerializer;
 
 public final class DefaultComponentSerializer {
     private static GsonComponentSerializer serializer = GsonComponentSerializer.builder()
-            .legacyHoverEventSerializer(NBTLegacyHoverEventSerializer.get())
-            .build();
+        .legacyHoverEventSerializer(NBTLegacyHoverEventSerializer.get())
+        .options(JSONOptions.byDataVersion().at(3680))
+        .build();
 
     public static GsonComponentSerializer get() {
         return serializer;
