74409ebd30f418de8f35c9042fbd47c98d605dd8
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2024-10-23 12:42:50 -0700

    1.21.3 publish

--- protocol/build.gradle.kts
+++ protocol/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
     `java-library`
 }
 
-version = "1.21.2"
+version = "1.21.3"
 description = "MCProtocolLib is a simple library for communicating with Minecraft clients and servers."
 
 repositories {
