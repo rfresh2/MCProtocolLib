9bc9afe58992b16c4ff945e54199e99e23f36789
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2025-02-15 18:17:50 -0800

    remove kqueue detection

--- protocol/src/main/java/org/geysermc/mcprotocollib/network/helper/TransportHelper.java
+++ protocol/src/main/java/org/geysermc/mcprotocollib/network/helper/TransportHelper.java
@@ -1,18 +1,16 @@
 package org.geysermc.mcprotocollib.network.helper;
 
 import io.netty.channel.epoll.Epoll;
-import io.netty.channel.kqueue.KQueue;
 import io.netty.incubator.channel.uring.IOUring;
 
 public class TransportHelper {
     public enum TransportMethod {
-        NIO, EPOLL, KQUEUE, IO_URING
+        NIO, EPOLL, IO_URING
     }
 
     public static TransportMethod determineTransportMethod() {
         if (isClassAvailable("io.netty.incubator.channel.uring.IOUring") && IOUring.isAvailable()) return TransportMethod.IO_URING;
         if (isClassAvailable("io.netty.channel.epoll.Epoll") && Epoll.isAvailable()) return TransportMethod.EPOLL;
-        if (isClassAvailable("io.netty.channel.kqueue.KQueue") && KQueue.isAvailable()) return TransportMethod.KQUEUE;
         return TransportMethod.NIO;
     }
 
