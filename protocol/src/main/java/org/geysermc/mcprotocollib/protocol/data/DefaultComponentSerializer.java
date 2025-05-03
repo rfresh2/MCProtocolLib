8c37cbc1ac1e93c1bd4efbbfe6c52cc2ccdf9d10
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2025-05-03 13:13:01 -0700

    simplify

--- protocol/src/main/java/org/geysermc/mcprotocollib/protocol/data/DefaultComponentSerializer.java
+++ protocol/src/main/java/org/geysermc/mcprotocollib/protocol/data/DefaultComponentSerializer.java
@@ -3,23 +3,12 @@ package org.geysermc.mcprotocollib.protocol.data;
 import net.kyori.adventure.text.serializer.gson.GsonComponentSerializer;
 import net.kyori.adventure.text.serializer.json.JSONOptions;
 import net.kyori.adventure.text.serializer.json.legacyimpl.NBTLegacyHoverEventSerializer;
-import net.kyori.option.OptionSchema;
 
 public final class DefaultComponentSerializer {
     private static GsonComponentSerializer serializer = GsonComponentSerializer.builder()
         .legacyHoverEventSerializer(NBTLegacyHoverEventSerializer.get())
-        .options(
-            OptionSchema.globalSchema().stateBuilder()
-                // after 1.16
-                .value(JSONOptions.EMIT_RGB, Boolean.TRUE)
-                .value(JSONOptions.EMIT_HOVER_EVENT_TYPE, JSONOptions.HoverEventValueMode.CAMEL_CASE)
-                .value(JSONOptions.EMIT_CLICK_EVENT_TYPE, JSONOptions.ClickEventValueMode.CAMEL_CASE)
-                // after 1.20.3
-                .value(JSONOptions.EMIT_COMPACT_TEXT_COMPONENT, Boolean.TRUE)
-                .value(JSONOptions.EMIT_HOVER_SHOW_ENTITY_ID_AS_INT_ARRAY, Boolean.TRUE)
-                .value(JSONOptions.VALIDATE_STRICT_EVENTS, Boolean.TRUE)
-                .build()
-        )
+        // From net.minecraft.SharedConstants.WORLD_VERSION
+        .options(JSONOptions.byDataVersion().at(3955))
         .build();
 
     public static GsonComponentSerializer get() {
