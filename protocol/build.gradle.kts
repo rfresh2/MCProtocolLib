cbc11025a7375a3291641ff5349f0f4fbd34c89e
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2025-09-24 01:23:57 -0700

    setup 1.21.9 build

--- protocol/build.gradle.kts
+++ protocol/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
     `java-library`
 }
 
-version = "1.21.8"
+version = "1.21.9"
 description = "MCProtocolLib is a simple library for communicating with Minecraft clients and servers."
 val javaVersion = JavaLanguageVersion.of(21)
 
