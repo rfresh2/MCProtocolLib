6f091dd573a854695336a38fd2deb7eae55c4d9c
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2025-11-11 22:15:51 -0800

    update deps

--- protocol/build.gradle.kts
+++ protocol/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("io.freefair.lombok") version "9.0.0"
+    id("io.freefair.lombok") version "9.1.0"
     `maven-publish`
     `java-library`
 }
@@ -42,9 +42,9 @@ dependencies {
 
     api("com.velocitypowered:velocity-native:3.4.0-SNAPSHOT")
 
-    api("org.checkerframework:checker-qual:3.51.1")
+    api("org.checkerframework:checker-qual:3.52.0")
 
-    testImplementation(platform("org.junit:junit-bom:6.0.0"))
+    testImplementation(platform("org.junit:junit-bom:6.0.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
