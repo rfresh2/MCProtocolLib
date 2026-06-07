0e49d4d359af16a27a545d19ffc2b9f34571543d
basaigh <53559772+basaigh@users.noreply.github.com>
2026-06-07 14:53:23 +0100

    26.2-snapshot-6

--- protocol/src/main/java/org/geysermc/mcprotocollib/protocol/codec/MinecraftCodec.java
+++ protocol/src/main/java/org/geysermc/mcprotocollib/protocol/codec/MinecraftCodec.java
@@ -236,8 +236,8 @@ import org.geysermc.mcprotocollib.protocol.packet.status.serverbound.Serverbound
 
 public class MinecraftCodec {
     public static final PacketCodec CODEC = PacketCodec.builder()
-            .protocolVersion((1 << 30) | 311)
-            .minecraftVersion("26.2 Snapshot 5")
+            .protocolVersion((1 << 30) | 312)
+            .minecraftVersion("26.2 Snapshot 6")
             .state(ProtocolState.HANDSHAKE, MinecraftPacketRegistry.builder()
                     .registerServerboundPacket(ClientIntentionPacket.class, ClientIntentionPacket::new)
             )
